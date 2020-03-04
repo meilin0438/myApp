8bdd43f2a8489f7fd68a2db457a6666db42ddd98
meilin <chinmeilin0438@gmail.com>
2020-03-04 11:59:54 +0800

    first_commit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,12 +1,36 @@
 package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundl
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton: Button =findViewById(R.id.roll_button)
+        rollButton.setOnClickListener{(rollDice())}
+
+        val countUpButton: Button =findViewById(R.id.countUpButton)
+        countUpButton.setOnClickListener{(rollcu())}
+    }
+
+    private fun rollDice(){
+        val rollText: TextView =findViewById(R.id.roll_text)
+        val randomInt = (1..6).random()
+        rollText.text=randomInt.toString()
+        Toast.makeText(this, "button clicked",Toast.LENGTH_SHORT).show()
+    }
+
+    private fun rollcu(){
+        val rollText: TextView =findViewById(R.id.roll_text)
+       // val randomInt = (1..6).random()
+        rollText.text= rollText.text + 1
+        Toast.makeText(this, "button clicked",Toast.LENGTH_SHORT).show()
     }
 }
